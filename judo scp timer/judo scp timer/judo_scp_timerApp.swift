b293442e66ea40ae1de6a5d717dbfed8bbaccf0f
Vera Dias <vera.ricardina.dias@gmail.com>
2025-10-29 23:28:57 +0000

    setup project

---
+++ judo scp timer/judo scp timer/judo_scp_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  judo_scp_timerApp.swift
+//  judo scp timer
+//
+//  Created by Vera Dias on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct judo_scp_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
